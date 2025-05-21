0cc0e1a66f69f21168e03c27dbd50edbb38d3d33
Marcos Bruno <contato@marcosbruno.com>
2025-05-21 08:35:06 +0100

    add logo

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,7 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.Handle("/", http.FileServer(http.Dir("./web/")))
+	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
 
 	srv := &http.Server{
 		Addr:    ":8080",
