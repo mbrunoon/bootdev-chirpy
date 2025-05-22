d65f57f7a62a410dd38d7595d445f6607e4a52a3
Marcos Bruno <contato@marcosbruno.com>
2025-05-22 08:25:58 +0100

    add metrics endpoints

--- internal/application/ApplicationHandler.go
+++ internal/application/ApplicationHandler.go
@@ -1,9 +1,25 @@
 package application
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func HealthzHandler(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	res.WriteHeader(http.StatusOK)
 	res.Write([]byte(`{"status": "OK"}`))
+}
+
+func (c *apiConfig) MetricsHandler(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/plain; charset=uft-8")
+	res.WriteHeader(http.StatusOK)
+	res.Write([]byte(fmt.Sprintf("Hits: %d", c.fileserverHits.Load())))
+}
+
+func (c *apiConfig) ResetMetricsHandler(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(http.StatusOK)
+	c.fileserverHits.Swap(0)
+	res.Write([]byte(fmt.Sprintf("Metrics reseted to %d", c.fileserverHits.Load())))
 }
--- internal/application/ApplicationRouter.go
+++ internal/application/ApplicationRouter.go
@@ -2,19 +2,39 @@ package application
 
 import (
 	"net/http"
+	"sync/atomic"
 
 	"github.com/gorilla/mux"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (c *apiConfig) middlewareMetricsInt(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		c.fileserverHits.Add(1)
+		next.ServeHTTP(res, req)
+	})
+}
+
 func NewRouter() *mux.Router {
 	mux := mux.NewRouter()
+	cfg := apiConfig{}
 
-	mux.Handle("/app", http.StripPrefix("/app", http.FileServer(http.Dir("./web/"))))
+	mux.Handle("/app", cfg.middlewareMetricsInt(
+		http.StripPrefix("/app", http.FileServer(http.Dir("./web/"))),
+	))
+
 	mux.PathPrefix("/app/assets").Handler(
-		http.StripPrefix("/app/assets", http.FileServer(http.Dir("./assets/"))),
+		cfg.middlewareMetricsInt(
+			http.StripPrefix("/app/assets", http.FileServer(http.Dir("./assets/"))),
+		),
 	)
 
 	mux.HandleFunc("/healthz", HealthzHandler)
+	mux.HandleFunc("/metrics", cfg.MetricsHandler)
+	mux.HandleFunc("/reset", cfg.ResetMetricsHandler)
 
 	return mux
 }
