27a4a6b28f6c669468c1787e2d7155e50f92ec47
Marcos Bruno <contato@marcosbruno.com>
2025-10-14 10:47:02 +0100

    CH4 JSON - completed

---
+++ helpers/json_response.go
@@ -0,0 +1,29 @@
+package helpers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func RespondWithJson(res http.ResponseWriter, code int, payload interface{}) {
+	res.Header().Add("Content-Type", "application/json")
+
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Fatalf("[error] json.Marshal(payload): %v", err)
+		res.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	res.WriteHeader(code)
+	res.Write(dat)
+}
+
+type JsonError struct {
+	Error string `json:"error"`
+}
+
+func RespondWithError(res http.ResponseWriter, code int, msg string) {
+	RespondWithJson(res, code, JsonError{Error: msg})
+}
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"slices"
+	"strings"
 	"sync/atomic"
+
+	"github.com/mbrunoon/bootdev-chirpy/helpers"
 )
 
 func main() {
@@ -15,6 +20,7 @@ func main() {
 	mux.Handle("/app/assets/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 
 	mux.HandleFunc("GET /api/healthz", healthzController)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpController)
 
 	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsController)
 	mux.HandleFunc("POST /admin/reset", apiCfg.resetController)
@@ -61,4 +67,44 @@ func healthzController(res http.ResponseWriter, req *http.Request) {
 	res.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	res.WriteHeader(http.StatusOK)
 	res.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func validateChirpController(res http.ResponseWriter, req *http.Request) {
+	res.Header().Add("Content-Type", "application/json")
+
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	type responseValid struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+
+	err := decoder.Decode(&params)
+	if err != nil {
+		helpers.RespondWithError(res, http.StatusInternalServerError, fmt.Sprintf("Error decoding %s", err))
+	}
+
+	if len(params.Body) > 140 {
+		helpers.RespondWithError(res, 400, "Chirp is too long")
+		return
+	}
+
+	helpers.RespondWithJson(res, http.StatusOK, responseValid{CleanedBody: cleanBody(params.Body)})
+}
+
+func cleanBody(body string) string {
+	badWords := []string{"kerfuffle", "sharbert", "fornax"}
+	cleanedBody := strings.Split(body, " ")
+
+	for i, w := range cleanedBody {
+		if slices.Contains(badWords, strings.ToLower(w)) {
+			cleanedBody[i] = "****"
+		}
+	}
+
+	return strings.Join(cleanedBody, " ")
 }
